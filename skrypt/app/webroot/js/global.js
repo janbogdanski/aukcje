d7497c14dbea5b20e6335d6fa4e2a61126aff82c
Jan Bogdanski <jan.b@aol.pl>
2012-12-16 10:58:07 +0100

    js: slug dynamiczny (moze byc irytujacy przy zmianie title ..)

--- skrypt/app/webroot/js/global.js
+++ skrypt/app/webroot/js/global.js
@@ -35,3 +35,23 @@ function picasaImgScaledUrl(url, size) {
     var split = url.lastIndexOf("/");
     return url.substring(0, split) + "/s" + size + url.substring(split);
 }
+
+var slug = function(str) {
+    if(str == undefined) return;
+    str = str.replace(/^\s+|\s+$/g, ''); // trim
+    str = str.toLowerCase();
+
+
+    // remove accents, swap ñ for n, etc
+    var from = "ąćęłńóśźż·/_,:;";
+    var to   = "acelnoszz------";
+    for (var i=0, l=from.length ; i<l ; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+
+    str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+        .replace(/\s+/g, '-') // collapse whitespace and replace by -
+        .replace(/-+/g, '-'); // collapse dashes
+
+    return str;
+};
