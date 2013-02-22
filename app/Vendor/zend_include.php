4206180349dd8a2d9bc06499622f5ca32eb886a3
Jan Bogdanski <jan.b@aol.pl>
2013-02-22 14:21:25 +0100

    gdata: skrypt includujacy Zend
    w akcjach wymagajacych zenda
    App::import('Vendor', 'zend_include_path');
    App::import('Vendor', 'Zend_Gdata', true, false, 'Zend/Gdata.php');

---
+++ app/Vendor/zend_include.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @author          Jan Bogdanski <janek.bogdanski@gmail.com>
+ *
+ * Creation date    22.02.13 14:08
+ */
+
+ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . dirname(__FILE__));
